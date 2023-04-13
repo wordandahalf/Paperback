668fd751dfa07beffe55173bfe0a5c09eb7773ca
wordandahalf <10679776+wordandahalf@users.noreply.github.com>
2023-04-13 17:44:49 -0400

    Fix issue with image conversion, autorotate when necessary

--- src/jvmMain/kotlin/image/Image.kt
+++ src/jvmMain/kotlin/image/Image.kt
@@ -9,41 +9,35 @@ import image.palette.BuiltinPalette
 import image.palette.Palette
 import java.awt.Color as AwtColor
 import java.nio.file.Path
-import kotlin.math.ceil
 
 object Image {
     const val WIDTH  = 600
     const val HEIGHT = 448
 
-    /*
-        // size = width x height x bitdepth
-        //      = 600 x 448 x 4 = 1,075,200 bits = 143,400 bytes
-     */
     fun convert(path: Path): ImmutableImage =
         ImmutableImage.loader().fromPath(path)
+            .let {
+                if (it.height > it.width)
+                    it.rotateRight()
+                else
+                    it
+            }
             .fit(WIDTH, HEIGHT, AwtColor.WHITE, ScaleMethod.Bicubic, Position.Center)
             .transform(DitheringTransform(BuiltinPalette, FloydSteinbergDiffusion))
 }
 
 fun ImmutableImage.toPackedArray(palette: Palette): ByteArray {
-    val length = this.width.times(this.height)
-    val data = ByteArray(length / 2)
+    val buffer = ByteArray(Image.WIDTH * Image.HEIGHT / 2)
 
-    for (i in 0 until length step 2) {
-        val x = i.mod(Image.WIDTH)
-        val y = i.floorDiv(Image.HEIGHT)
-
-        if (x >= this.width)
-            continue
-
-        if (y >= this.height)
-            continue
+    for (i in buffer.indices) {
+        val x = i.mod(Image.WIDTH / 2) * 2
+        val y = i / (Image.WIDTH / 2)
 
         val first = palette.index(Color(this.pixel(x, y).argb.and(0x00FFFFFF)))!!
         val second = palette.index(Color(this.pixel(x + 1, y).argb.and(0x00FFFFFF)))!!
 
-        data[i.floorDiv(2)] = first.shl(8).or(second).toByte()
+        buffer[i] = second.shl(4).or(first).toByte()
     }
 
-    return data
+    return buffer
 }
